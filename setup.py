72ad153ab3d2bdf0ff90d1639a7de8f09431a687
jlux <jerome.lux78@gmail.com>
2020-12-15 15:24:38 +0100

    change PIL to Pillow

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(name='pymosaic',
       description='Module to create image mosaic',
       packages=find_packages(include=['pymosaic','pymosaic.*']),
       install_requires=['numpy>=1.18.5',
-                        'PIL',
+                        'Pillow',
                         'numba>=0.50.1']
       )
